bdfe23466ff4e4ab78f95e181c1a481120cf3913
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-26 20:46:39 -0300

    feat: enhance zod validation to use on form

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,11 +1,29 @@
 import { z } from "zod";
 
 export const productSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  price: z.number(),
-  image: z.string().optional(),
-  category: z.string(),
+  name: z.string().min(1, { message: "O nome é um campo obrigatório!" }),
+  description: z
+    .string()
+    .min(1, { message: "A descrição do produto é um campo obrigatório!" }),
+  price: z
+    .number({
+      invalid_type_error: "Preço inválido!",
+      required_error: "O preço é um campo obrigatório!",
+    })
+    .min(1, { message: "O preço deve ser no mínimo 1,00." }),
+  image: z
+    .string()
+    .url({ message: "URL Inválida" })
+    .optional()
+    .nullable()
+    .or(z.literal(""))
+    .transform((image) => {
+      if (image === "") return null;
+      return image;
+    }),
+  category: z
+    .string()
+    .min(1, { message: "É necessário especificar uma categoria!" }),
 });
 
 export type IProduct = z.infer<typeof productSchema>;
