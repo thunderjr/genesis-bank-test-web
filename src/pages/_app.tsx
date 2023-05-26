2416bf25ab80884b5a683c9ed17b629b35bda186
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-26 17:03:31 -0300

    feat: add navbar to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { NavBar } from "@/components/nav-bar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
