707352464d599038a32db55ac9fd1199ddd6b442
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-27 12:25:43 -0300

    chore: adjust category filters

--- src/components/category-filter-buttons.tsx
+++ src/components/category-filter-buttons.tsx
@@ -10,12 +10,13 @@ export const CategoryFilterButtons = ({
   handleSearch,
 }: Props) => {
   return (
-    <div className="self-center mb-6">
+    <div className="self-center md:self-start mb-6">
+      <h4>Filtrar:</h4>
       {categories.map((category, i) => (
         <span
           key={`category-${i}`}
           onClick={() => handleSearch(category)}
-          className="inline-block cursor-pointer text-lg font-bold bg-white bg-opacity-50 rounded-full px-4 text-black mx-1"
+          className="inline-block cursor-pointer text-lg font-bold bg-white bg-opacity-50 px-4 text-black mx-1"
         >
           {category}
         </span>
