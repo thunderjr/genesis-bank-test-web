523b25e652a30f33f640d2ea8fc476ef09c6eca8
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-27 00:38:30 -0300

    feat: adapt axios fetcher to receive query params key as object and parse to querystring

--- src/helpers/axios-fetcher.ts
+++ src/helpers/axios-fetcher.ts
@@ -1,4 +1,12 @@
 import axios from "axios";
 
-export const axiosFetcher = (url: string) =>
-  axios.get(url).then((res) => res.data);
+export const axiosFetcher = async ([url, searchParams = {}]: [
+  url: string,
+  searchParams: Record<string, string>
+]) => {
+  const nonUndefinedKeys = JSON.parse(JSON.stringify(searchParams));
+  const queryParams = new URLSearchParams(nonUndefinedKeys || {}).toString();
+  return axios
+    .get(url + (queryParams ? `?${queryParams}` : ""))
+    .then((res) => res.data);
+};
