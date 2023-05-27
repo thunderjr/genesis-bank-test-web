2386d85eac41123fbe872ed96b6c02472655fc43
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-27 01:00:08 -0300

    fix: remove extra margin on products form

--- src/pages/products/new.tsx
+++ src/pages/products/new.tsx
@@ -2,7 +2,7 @@ import { ProductForm } from "@/components/product-form";
 
 export default function NewProductPage() {
   return (
-    <main className="h-full md:-mt-16 flex items-center justify-center">
+    <main className="h-full flex items-center justify-center">
       <section className="content p-12 px-24 bg-slate-700 backdrop-blur-md bg-opacity-40">
         <ProductForm />
       </section>
