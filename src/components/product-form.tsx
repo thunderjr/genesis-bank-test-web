3c7fc15106fcca4b394fa012e1065e6b35290f59
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-05-27 00:40:55 -0300

    chore: default value to image input to avoid searching for another image

--- src/components/product-form.tsx
+++ src/components/product-form.tsx
@@ -16,6 +16,8 @@ export const ProductForm = () => {
   } = useForm<IProduct>({
     defaultValues: {
       category: "Default",
+      image:
+        "https://img.freepik.com/free-vector/white-product-podium-with-green-tropical-palm-leaves-golden-round-arch-green-wall_87521-3023.jpg",
     },
     resolver: zodResolver(productSchema),
   });
